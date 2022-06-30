fb18b92a583905031f7240ac1a72c46d05bf0157
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-06-30 18:44:44 -0300

    Add files via upload

--- OrientObjPY/URL.py
+++ OrientObjPY/URL.py
@@ -10,20 +10,46 @@ print(base)
 parametros = url[p+1:]
 print("Parametros da URL: ", parametros)
 
-parametro_moeda= "moedaOrigem"
-moeda_origem = url.find(parametro_moeda)
-moeda_origem = moeda_origem + len(parametro_moeda)
-moeda_origem_final = url[moeda_origem+1:]
-print("Moeda origem: ",moeda_origem_final)
+parametro_oo = "moedaDestino"
+indice = url.find(parametro_oo)
+posicao = indice + len(parametro_oo) + 1
+parador = url.find("&")
+if parador == -1:
+    print( url[posicao:])
 
-parametro_destino= "moedaDestino"
-parador= url.find("&")
+else:
+    print(url[posicao:parador])
 
-Destino = url.find(parametro_destino)
-Posicao = Destino + len(parametro_destino)
-moeda_destino=url[Posicao+1:parador]
 
-print("Moeda destino: ",moeda_destino)
+
+
+
+
+
+
+
+
+#parametro_moeda= "moedaOrigem"
+#moeda_origem = url.find(parametro_moeda)
+#moeda_origem = moeda_origem + len(parametro_moeda)
+#parador= url.find("&")
+#if parador == -1:
+    #x = url[moeda_origem:]
+    #print(x,"+++++")
+#else:
+    #x =url[moeda_origem:parador]
+    #print(x,"------")
+
+#print("Moeda origem: ",moeda_origem_final)
+
+#parametro_destino= "moedaDestino"
+#parador= url.find("&")
+
+#Destino = url.find(parametro_destino)
+#Posicao = Destino + len(parametro_destino)
+#moeda_destino=url[Posicao+1:parador]
+
+#print("Moeda destino: ",moeda_destino)
 
 
 
