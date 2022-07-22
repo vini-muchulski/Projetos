e11c0d78becb9eae47ed0b192ad6dc0e0d436b42
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-07-21 22:57:20 -0300

    Add files via upload

--- OrientObjPY/cpf.py
+++ OrientObjPY/cpf.py
@@ -1,1 +1,13 @@
 class Cpf:
+    def __init__(self, documento):
+        documento = str(documento)
+        if self.cpf_valido(documento):
+         self.cpf = documento
+        else:
+         raise ValueError("Erro de CPF")
+
+    def cpf_valido(self, documento):
+        if len(documento) == 11:
+            return True
+        else:
+            return False
--- OrientObjPY/principal.py
+++ OrientObjPY/principal.py
@@ -1,5 +1,11 @@
+from cpf import Cpf
 #cpf = str(input('Digite um CPF'))
 cpf = "25634614627"
-cpf = str(cpf)
-tamanho = len(cpf)
-print(tamanho)
+
+fatia_cpf1 = cpf[:3]
+fatia_cpf2 = cpf[3:6]
+fatia_cpf3 = cpf[6:9]
+fatia_cpf4 = cpf[9:]
+
+print(fatia_cpf4)
+objeto_cpf = Cpf(cpf)
