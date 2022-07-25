3146a00e802bd0806a79887fb30c1bf287e401d5
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-07-25 11:26:37 -0300

    Add files via upload

--- OrientObjPY/cpf.py
+++ OrientObjPY/cpf.py
@@ -5,9 +5,23 @@ class Cpf:
          self.cpf = documento
         else:
          raise ValueError("Erro de CPF")
+    def __str__(self):
+        return self.mascara_cpf()
 
     def cpf_valido(self, documento):
         if len(documento) == 11:
             return True
         else:
             return False
+
+    def mascara_cpf(self):
+        fatia_cpf1 = self.cpf[:3]
+        fatia_cpf2 = self.cpf[3:6]
+        fatia_cpf3 = self.cpf[6:9]
+        fatia_cpf4 = self.cpf[9:]
+
+        return( "{}.{}.{}-{}".format(
+            fatia_cpf1,
+            fatia_cpf2,
+            fatia_cpf3,
+            fatia_cpf4))
--- OrientObjPY/principal.py
+++ OrientObjPY/principal.py
@@ -1,11 +1,9 @@
 from cpf import Cpf
 #cpf = str(input('Digite um CPF'))
-cpf = "25634614627"
-
-fatia_cpf1 = cpf[:3]
-fatia_cpf2 = cpf[3:6]
-fatia_cpf3 = cpf[6:9]
-fatia_cpf4 = cpf[9:]
-
-print(fatia_cpf4)
+cpf = "55634614627"
 objeto_cpf = Cpf(cpf)
+
+print(objeto_cpf)
+
+
+
