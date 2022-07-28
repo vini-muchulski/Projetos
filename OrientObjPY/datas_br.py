3e2587db4b9d7a612e40da5f926491f96910c21d
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-07-28 20:03:33 -0300

    Add files via upload

--- OrientObjPY/datas_br.py
+++ OrientObjPY/datas_br.py
@@ -3,8 +3,27 @@ from datetime import datetime, timedelta
 class Dia:
     def __init__(self):
         self.dia_cadastro = datetime.today()
-        self.dia_cadastro = str(self.dia_cadastro)
-
+        self.momento_do_cadastro = (self.dia_cadastro.strftime("%d/%m/%Y %H:%M"))
 
     def __str__(self):
-        return self.dia_cadastro
+        return self.momento_do_cadastro
+
+    def mes_cadastro(self):
+        meses = [
+            "Janeiro", "Fevereiro", "Março",
+            "Abril", "Maio", "Junho",
+            "Julho", "Agosto", "Setembro",
+            "Outubro", "Novembro", "Dezembro"
+        ]
+        mes_cadastro = self.dia_cadastro.month
+        return meses[mes_cadastro-1]
+
+
+    def semana_dia (self):
+        semana_dia_lista = [
+            "segunda", "terça",
+            "quarta", "quinta", "sexta",
+            "sábado", "domingo"
+        ]
+        semana_dia = self.dia_cadastro.weekday()
+        return semana_dia_lista[semana_dia]
--- OrientObjPY/date_time.py
+++ OrientObjPY/date_time.py
@@ -2,12 +2,17 @@ import time
 from datetime import datetime, timedelta
 from datas_br import Dia
 
-print(datetime.today())
+#print(datetime.today())
 
 cadastro = Dia()
 
-print(cadastro)
+print(cadastro.mes_cadastro())
+print(cadastro.semana_dia())
 
-time.sleep(2)
+
 cadastro = Dia()
 print(cadastro)
+
+#hoje = datetime.today()
+#hoje_format = hoje.strftime("%d/%m/%Y %H:%M")
+#print(hoje_format)
