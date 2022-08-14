33b7ca1efb294c74d42b0f780657164d88e5efcb
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-08-14 18:11:43 -0300

    Add files via upload

---
+++ CursoEmVideo/Mundo 3/ex072.py
@@ -0,0 +1,7 @@
+#Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+tupla = ("Zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","dezesseis","dezessete","dezoito","dezenove","vinte")
+
+input = int(input("Digite um valor entre 0 e 20: "))
+
+print("Você digitou ", tupla[input].title())
