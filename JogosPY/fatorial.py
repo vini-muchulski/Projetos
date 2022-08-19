ed3527d367d4f5d4c4332902a861ef1795082582
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-08-19 15:32:18 -0300

    Add files via upload

--- JogosPY/fatorial.py
+++ JogosPY/fatorial.py
@@ -4,15 +4,12 @@ numero = int(input("Digite o valor do numero: "))
 fatorial = numero
 restou = 0
 
-while True:
-     if  numero >1:
-          print("------", numero)
-          numero -=1
-          fatorial = fatorial * (numero)
-          restou = fatorial
+while numero != 1:
+     print("------", numero)
+     numero -=1
+     fatorial = fatorial * (numero)
+     restou = fatorial
 
-     if numero == 1:
-          break
 
 print(restou)
 
