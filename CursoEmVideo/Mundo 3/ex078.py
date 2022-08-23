0e89426e694cc3fc6f6628df4a4d7e3acba9d9e4
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-08-23 17:18:33 -0300

    Add files via upload

---
+++ CursoEmVideo/Mundo 3/ex078.py
@@ -0,0 +1,31 @@
+#Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+lista =[]
+
+
+for c in range(0,5):
+
+    numero = int(input("Digite um valor para a posição {} : ".format(c)))
+    lista.append(numero)
+
+    """continua = str(input("Deseja continuar? [S/N]: "))
+    continua = continua.upper()
+
+    if continua != "S":
+        break"""
+
+
+
+maior_valor = sorted(lista)
+menor_valor = sorted(lista, reverse=True)
+
+print("-="*20)
+print(lista)
+print(f"O maior valor foi {maior_valor[0]}  e menor foi {menor_valor[0]}")
+
+for indice, valor in enumerate(lista):
+    if valor == maior_valor[0]:
+        print(indice)
+
+    if valor == menor_valor:
+        print(indice)
+
