597a8d56324ed00b770e76011b42dbe9ea9999c7
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-08-24 15:38:47 -0300

    Add files via upload

--- CursoEmVideo/Mundo 3/ex083.py
+++ CursoEmVideo/Mundo 3/ex083.py
@@ -1,3 +1,21 @@
 #Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
-expressao = input("Digite o valor da expressão:")
+expressao = str(input("Digite o valor da expressão:"))
+lista = []
+
+for carc in expressao:
+    if carc =="(":
+        lista.append("(")
+    if carc ==")":
+        if len(lista)>0:
+            lista.pop() #pop remove o ultimo elemento de uma lista
+        else:
+            lista.append(")")
+            break
+
+
+if len(lista)== 0:
+    print("Expressão VÁLIDA!")
+
+else:
+    print("Expressão INVÁLIDA!")
