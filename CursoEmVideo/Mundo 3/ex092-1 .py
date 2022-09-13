f5c1036a9465f530b9d78220873dbdb087f237ef
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-13 09:06:48 -0300

    Add files via upload

---
+++ CursoEmVideo/Mundo 3/ex092-1 .py
@@ -0,0 +1,28 @@
+#Exercício Python 092: Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+from datetime import datetime
+
+ano_atual = datetime.now().year
+
+infos = {}
+infos['Nome'] = str(input("Digite o nome: ")).title()
+infos['Idade: '] = ano_atual - int(input("Qual ano de nascimento: "))
+ctps = int(input("Número da CTPS (0 não tem): "))
+
+if ctps == 0:
+    infos['Carteira de trabalho: '] = "0"
+    print("-=" * 30)
+    print(f" Nome: {infos['Nome']} \n Idade: {infos['Idade: ']} \n CTPS = {ctps}")
+
+else:
+    infos['Ano_Contratacao'] = int(input("Qual o ano de contratação? "))
+    infos['Aposentadora'] = infos['Ano_Contratacao'] + 35
+    print("-=" * 30)
+    print(f" Nome: {infos['Nome']} \n Idade: {infos['Idade: ']} anos \n CTPS = {ctps} \n Ano de Contratação = {infos['Ano_Contratacao']} \n Ano de Aposentadoria = {infos['Aposentadora']}")
+
+
+
+
+
+
+
+
