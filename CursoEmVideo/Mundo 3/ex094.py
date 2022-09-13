d3c341517f5263c4b1788061b00b2bd3f6f8e866
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-13 10:33:02 -0300

    Add files via upload

--- CursoEmVideo/Mundo 3/ex094.py
+++ CursoEmVideo/Mundo 3/ex094.py
@@ -1,8 +1,9 @@
 #Exercício Python 094: Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: A) Quantas pessoas foram cadastradas B) A média de idade C) Uma lista com as mulheres D) Uma lista de pessoas com idade acima da média
+from pydoc import visiblename
+
+
 infos = {}
-infos['Nome'] = []
-infos['Sexo'] = []
-infos['Idade'] = []
+lista_final = list()
 
 nome = 0
 idade = 0
@@ -11,23 +12,24 @@ cont = 0
 media_idades = 0
 
 while True:
-    nome = str(input("Digite o nome: "))
-    idade = int(input("Digite a idade: "))
-    sexo = str(input("Digite o genero (M/F): "))
+    
+    infos['Nome'] = str(input("Digite o nome: "))
+    infos['Idade'] = int(input("Digite a idade: "))
+    infos['Sexo'] = str(input("Digite o genero (M/F): "))
+
+    if infos['Sexo'] not in "MmFf":  
+        while True:
+            print("ERRO! Insira novamente!")
+            infos['Sexo'] = str(input("Digite o genero (M/F): "))
+            if infos['Sexo'] in "MmFf":
+                break
+
     cont +=1
-    media_idades +=idade
-
-    if sexo not in "MmFf":
-        print("ERRO! Insira novamente!")
-        sexo = str(input("Digite o genero (M/F): "))
-
-    infos['Nome'].append(nome)
-    infos['Sexo'].append(sexo)
-    infos['Idade'].append(idade)
+    media_idades +=infos['Idade']
+    lista_final.append(infos.copy())
 
     continua = str(input("Deseja continuar? [S/N]: "))
     continua = continua.upper()
-
     if continua != "S":
         break
 
@@ -35,7 +37,7 @@ media_final = media_idades/cont
 
 print(f"Ao todo {cont} pessoas foram cadastradas")
 print(f"A média foi {media_final} anos")
-print(infos)
+print(lista_final)
     
         
 
