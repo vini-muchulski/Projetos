7b9b9310758087ee4f2b6e6d9b5ae977990e89c7
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-12 21:40:00 -0300

    Add files via upload

--- CursoEmVideo/Mundo 3/ex091.py
+++ CursoEmVideo/Mundo 3/ex091.py
@@ -1,5 +1,7 @@
 #Exercício Python 091: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 import random
+from operator import itemgetter
+from time import sleep
 
 jogadores = {"Jogador 1": random.randint(1,6),
              "Jogador 2": random.randint(1,6),
@@ -8,7 +10,13 @@ jogadores = {"Jogador 1": random.randint(1,6),
 
 
 
-for k in sorted(jogadores, key=jogadores.get, reverse=True):
-    print(f"{k} = ", jogadores[k])
+rank = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+print(rank)
 
-print(jogadores)
+p = 0
+for i,v in rank:
+    p +=1
+    print(f"{p}° lugar - ",i, "==",v)
+    sleep(1)
+
+
