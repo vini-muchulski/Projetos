df68e9cc00ff6bc4e2a9d3df600ed586b7d816bf
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-30 19:28:16 -0300

    Add files via upload

---
+++ CursoEmVideo/Mundo 3/ex099.py
@@ -0,0 +1,22 @@
+"Exercício Python 099: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior."
+
+def maior(n1,n2,n3):
+
+    maior = n1
+    menor = n1
+
+    if n2>n1 and n2>n3:
+        maior = n2
+
+    if n3>n1 and n3>n2:
+        maior = n3
+
+    if n2<n1 and n2<n3:
+        menor = n2
+
+    if n3 < n1 and n2 > n3:
+        menor = n3
+
+    print(f"O menor é {menor} e o maior é {maior}")
+
+maior(5,3,4)
