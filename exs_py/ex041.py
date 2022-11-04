f857dfaf06d88d105cee6617eab8005e56f97b57
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-11-04 16:44:38 -0300

    Add files via upload

---
+++ exs_py/ex041.py
@@ -0,0 +1,10 @@
+"Remova o elemento ‘Carlos’ da lista nomes:"
+
+
+nomes = ['vini', 'levi','Carlos', 'cipher', 'Trigger']
+
+print(nomes)
+
+nomes.remove("Carlos")
+
+print(nomes)
---
+++ exs_py/ex042.py
@@ -0,0 +1,9 @@
+"Mostre o segundo, terceiro e quarto elemento da lista nomes. Separadamente, mostre apenas o último elemento da lista nomes:"
+
+nomes = ['vini', 'levi', 'cipher', 'Trigger']
+
+print(nomes)
+
+print(f"Nome do segundo ao quarto = {nomes[1:4]}")
+
+print(f"Ultimo elemento = {nomes[-1]}")
